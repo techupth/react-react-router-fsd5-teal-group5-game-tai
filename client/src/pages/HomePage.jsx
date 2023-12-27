dd032b5207a566c1e69a11d5ca69c09c865cff87
Tai <5506726tai@gmail.com>
2023-12-27 16:13:24 +0700

    ex 1 and 2 fin but not 3 for now

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
   const [products, setProducts] = useState([]);
@@ -21,16 +22,20 @@ function HomePage() {
   useEffect(() => {
     getProducts();
   }, []);
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
-        <button>Create Product</button>
+        <button onClick={() => navigate("/product/create")}>
+          Create Product
+        </button>
       </div>
       <div className="product-list">
-        {products.map((product) => {
+        {products.map((product, index) => {
           return (
-            <div className="product">
+            <div className="product" key={index}>
               <div className="product-preview">
                 <img
                   src="https://via.placeholder.com/250/250"
@@ -44,8 +49,18 @@ function HomePage() {
                 <h2>Product price: {product.price}</h2>
                 <p>Product description: {product.description} </p>
                 <div className="product-actions">
-                  <button className="view-button">View</button>
-                  <button className="edit-button">Edit</button>
+                  <button
+                    className="view-button"
+                    onClick={() => navigate(`/product/view/${product.id}`)}
+                  >
+                    View
+                  </button>
+                  <button
+                    className="edit-button"
+                    onClick={() => navigate("/pages/EditProductPage")}
+                  >
+                    Edit
+                  </button>
                 </div>
               </div>
 
--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
 function ViewProductPage() {
+  const [page, setPage] = useState({});
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const getPage = async () => {
+    const results = await axios.get(`	
+    http://localhost:4001/products/${params.productId}`);
+    setPage(results.data.data);
+  };
+
+  useEffect(() => {
+    getPage();
+  }, []);
+
   return (
     <div>
       <h1>View Product Page</h1>
       <div className="view-product-container">
-        <h2>Product Title</h2>
-        <p>Content</p>
+        <h2>{page.name}</h2>
+        <p>{page.description}</p>
       </div>
-      <button>Back to Home</button>
+      <button onClick={() => navigate("/")}>Back to Home</button>
     </div>
   );
 }
